eb133e5516674b4ba93957ad43aa9acf4b9b0c2b
Jeroen Valcke <valcke_jeroen@hotmail.com>
2018-05-15 17:32:27 +0200

    Add type selection to CKE slider plugin

--- public/app/plugins/image-slider.js
+++ public/app/plugins/image-slider.js
@@ -11,6 +11,46 @@
 				CKEditorConfigPack,
 				DialogService
 			) {
+				var updateWidget = function(data, element) {
+					if (!_.get(data, "images.length")) {
+						return;
+					}
+
+					var container = element.getChild(0);
+					var type = data.type || "slider";
+					var wrapperClass = "wcm-" + type;
+					var containerClass = "wcm-" + type + "__images";
+					var slideClass = type === "gallery" ? "wcm-gallery__item" : "wcm-slider__slide";
+
+					function addPrecedingSlash(str) {
+						return str ? (str.charAt(0) === "/" ? str : "/" + str) : str;
+					}
+
+					element.$.className = wrapperClass;
+					container.$.className = containerClass;
+
+					container.setHtml(_.map(data.images, function(image) {
+						var uuid = image.value.original.asset.uuid;
+						var copyright = image.value.meta.copyright;
+						var description = image.value.meta.description;
+						var title = image.value.meta.title;
+						var croppedUrl = addPrecedingSlash(image.value.cropped.asset.url);
+						var originalUrl = image.value.original.asset.url ? addPrecedingSlash(image.value.original.asset.url) : "/files/download/" + uuid;
+
+						return [
+							"<div class=\"" + slideClass + "\"",
+							"style=\"background-image: url('" + croppedUrl + "');\"",
+							"data-src=\"" + croppedUrl + "\"",
+							"data-uuid=\"" + uuid + "\"",
+							"data-original-src=\"" + originalUrl + "\"",
+							"data-copyright=\"" + copyright + "\"",
+							"data-description=\"" + description + "\"",
+							"data-title=\"" + title + "\"",
+							"></div>",
+						].join(" ");
+					}).join(""));
+				};
+
 				return {
 					meta: {
 						toolbar: [{
@@ -21,37 +61,6 @@
 					},
 					plugin: {
 						init: function(editor) {
-							var updateWidget = function(images, container) {
-								if (!images.length) {
-									return;
-								}
-
-								function addPrecedingSlash(str) {
-									return str ? (str.charAt(0) === "/" ? str : "/" + str) : str;
-								}
-
-								container.setHtml(images.map(function(image) {
-									var uuid = image.value.original.asset.uuid;
-									var copyright = image.value.meta.copyright;
-									var description = image.value.meta.description;
-									var title = image.value.meta.title;
-									var croppedUrl = addPrecedingSlash(image.value.cropped.asset.url);
-									var originalUrl = image.value.original.asset.url ? addPrecedingSlash(image.value.original.asset.url) : "/files/download/" + uuid;
-
-									return [
-										"<div class=\"wcm-slider__slide\"",
-										"style=\"background-image: url('" + croppedUrl + "');\"",
-										"data-src=\"" + croppedUrl + "\"",
-										"data-uuid=\"" + uuid + "\"",
-										"data-original-src=\"" + originalUrl + "\"",
-										"data-copyright=\"" + copyright + "\"",
-										"data-description=\"" + description + "\"",
-										"data-title=\"" + title + "\"",
-										"></div>",
-									].join(" ");
-								}).join(""));
-							};
-
 							editor.widgets.add("imageSlider", {
 								template: [
 									"<div class=\"wcm-slider\">",
@@ -59,22 +68,30 @@
 									"<div class=\"wcm-slider__slide\" data-placeholder=\"true\" style=\"background-image: url('" + CKEditorConfigPack.assetsDirPath + "img/image.png');\"></div>", // eslint-disable-line
 									"</div>", // eslint-disable-line
 									"</div>",
-
 								].join(""),
 								allowedContent: "div[data-*]",
 								upcast: function(el) {
-									return el.name === "div" && el.hasClass("wcm-slider");
+									return el.name === "div" && (
+										el.hasClass("wcm-slider") ||
+										el.hasClass("wcm-gallery")
+									);
 								},
 								downcast: function() {
-									updateWidget(this.data.images, this.element.getChild(0));
+									updateWidget(this.data, this.element);
 								},
 								init: function() {
 									var widget = this;
 									var data = {
 										images: [],
 									};
+									var typeOptions = [
+										{ value: "slider", label: "Slider" },
+										{ value: "gallery", label: "Gallery" },
+									];
 									var newData = null;
 									var images = widget.element.getChild(0).getChildren();
+
+									data.type = widget.element.hasClass("wcm-slider") ? typeOptions[0].value : typeOptions[1].value;
 
 									function removePrecedingSlash(str) {
 										return str ? (str.charAt(0) === "/" ? str.slice(1) : str) : str;
@@ -124,16 +141,19 @@
 									}
 
 									widget.setData("images", data.images);
+									widget.setData("type", data.type);
 
 									widget.on("edit", function() {
 										newData = angular.copy(this.data);
+										newData.typeOptions = typeOptions;
 
 										DialogService.openModal({
 											templateUrl: CKEditorConfigPack.modulePath + "templates/sliderModal.tpl.html",
 											data: newData,
 										}).then(function() {
 											widget.setData("images", newData.images);
-											updateWidget(newData.images, widget.element.getChild(0));
+											widget.setData("type", newData.type);
+											updateWidget(newData, widget.element);
 											editor.fire("change");
 										});
 									});
